b7e587455365f72573e830d726a1cdbcc9e6353f
mont29 <montagne29@wanadoo.fr>
2012-02-21 21:46:46 +0100

    Updated Hunspell (can now handle 'recursive' classes). Updated fr dic to latest one from http://www.dicollecte.org/, 'Classique & Réforme 1990 4.4.1 — 20 décembre 2011'.

--- kernel/hunspell.py
+++ kernel/hunspell.py
@@ -34,7 +34,6 @@ import re
 
 
 ZIP_DICS = os.path.join(os.path.dirname(__file__), "dics.zip")
-MAXLEN = 10000  # XXX We assume no generated word can be longer than that!
 
 
 class Hunspell(object):
@@ -45,6 +44,10 @@ class Hunspell(object):
 
     def __init__(self):
         self.reset()
+
+    def _get_ids(self):
+        return self.dics.keys()
+    ids = property(_get_ids, doc="ids of dics currently loaded")
 
     def reset(self, idname=None):
         if idname:
@@ -96,6 +99,51 @@ class Hunspell(object):
                 lines = zip_arch.open(dic)
                 self.parse_dic(self.dics[idname], bytes2str(lines))
 
+    # -------------------------------------------------------------------------
+    # Parsing!
+
+    # First, two helper funcs.
+    @staticmethod
+    def _classes_split(fmode, clss):
+        if fmode in {"ASCII", "UTF-8"}:
+            return clss
+        elif fmode == "long":
+            ln = len(clss)
+            # XXX This will cut last char in case of odd number...
+            #     But this is not supposed to happen!
+            return (clss[i:i+2] for i in range(0, ln, 2))
+        elif fmode == "num":
+            return clss.split(',')
+
+    @staticmethod
+    def _classes_preprocess(af, clss):
+        """
+        Generate all possible combinaisons for a given set of classes.
+        """
+        spfx = []
+        cpfx = [None]
+        ssfx = []
+        csfx = [None]
+        for c in clss:
+            if c not in af:
+                continue
+            afc = af[c]
+            if afc["sfx"]:
+                if afc["crossp"]:
+                    csfx.append(c)
+                else:
+                    ssfx.append((c, None))
+            else:
+                if afc["crossp"]:
+                    cpfx.append(c)
+                else:
+                    spfx.append((c, None))
+        ret = list(itertools.product(cpfx, csfx))
+        ret.remove((None, None))
+        ret += spfx + ssfx
+        return ret
+
+    # And now, real parser funcs.
     def parse_aff(self, dic, lines):
         """
         Parse an .aff hunspell file, handling only a subset of features.
@@ -131,12 +179,13 @@ class Hunspell(object):
 
         af_map = dic["af_map"]
         af_classes = dic["af_classes"]
+        flag_mode = dic["flag_mode"]
         for l in lines:
             l = l.split()
             if not l:
                 continue
             if l[0] == "FLAG":
-                dic["flag_mode"] = l[1]
+                flag_mode = dic["flag_mode"] = l[1]
             elif l[0] == "AF":
                 if af:
                     af_map[str(af)] = l[1]
@@ -156,61 +205,38 @@ class Hunspell(object):
                     af_classes[curr]["rules"] = []
                 else:
                     # Parse/add a new rule to current class.
-                    # XXX We do not handle "recursive" affixes here!
                     if l[2] == "0":
                         l[2] = ""
-                    if l[3] == "0":
-                        l[3] = ""
-                    elif '\\' in l[3]:
-                        l[3] = l[3].replace(r"\\", "##")
-                        l[3] = l[3].replace('\\', '')
-                        l[3] = l[3].replace("##", '\\')
-                    r = {"strip": l[2], "add": (l[3], []),
+                    # XXX This does not handle escaped '/'!
+                    l3 = l[3].split('/')
+                    l3[0] = l3[0].split()[0]
+                    # We have to delay pre-processing of classes here,
+                    # as they are not yet all defined!
+                    if len(l3) == 1:
+                        l3.append("")
+                    if l3[0] == "0":
+                        l3[0] = ""
+                    elif '\\' in l3[0]:
+                        l3[0] = l3[0].replace(r"\\", "##")
+                        l3[0] = l3[0].replace('\\', '')
+                        l3[0] = l3[0].replace("##", '\\')
+                    r = {"strip": l[2], "add": [l3[0], l3[1]],
                          "if": _if_preprocess(l[4], af_classes[curr]["sfx"])}
                     af_classes[curr]["rules"].append(r)
+
+        # Now, we can pre-process "recursive" classes calls found in
+        # classes' rules.
+        for c in af_classes.values():
+            for r in c["rules"]:
+                clss = r["add"][1]
+                clss = af_map.get(clss, clss)
+                clss = self._classes_split(flag_mode, clss)
+                r["add"][1] = self._classes_preprocess(af_classes, clss)
 
     def parse_dic(self, dic, lines):
         """
         Parse a .dic hunspell file.
         """
-        def _classes_split(fmode, clss):
-            if fmode in {"ASCII", "UTF-8"}:
-                return clss
-            elif fmode == "long":
-                ln = len(clss)
-                # XXX This will cut last char in case of odd number...
-                #     But this is not supposed to happen!
-                return (clss[i:i+2] for i in range(0, ln, 2))
-            elif fmode == "num":
-                return clss.split(',')
-
-        def _classes_preprocess(af, clss):
-            """
-            Generate all possible combinaisons for a given set of classes.
-            """
-            spfx = []
-            cpfx = [None]
-            ssfx = []
-            csfx = [None]
-            for c in clss:
-                if c not in af:
-                    continue
-                afc = af[c]
-                if afc["sfx"]:
-                    if afc["crossp"]:
-                        csfx.append(c)
-                    else:
-                        ssfx.append((c, None))
-                else:
-                    if afc["crossp"]:
-                        cpfx.append(c)
-                    else:
-                        spfx.append((c, None))
-            ret = list(itertools.product(cpfx, csfx))
-            ret.remove((None, None))
-            ret += spfx + ssfx
-            return ret
-
         first_l = True
         flag_mode = dic["flag_mode"]
         af_map = dic["af_map"]
@@ -224,39 +250,49 @@ class Hunspell(object):
                     continue
             if not l or l.startswith('\t'):
                 continue
-            # XXX This is not conform to specs... But seems to work with
-            #     most dics for now!
+            # XXX This does not handle escaped '/'!
             l = l.split('/')
             l[0] = l[0].split()[0]
             classes = []
             if len(l) > 1:
                 l[1] = l[1].split()[0]
                 l[1] = af_map.get(l[1], l[1])
-                l[1] = _classes_split(flag_mode, l[1])
-                classes = _classes_preprocess(af_classes, l[1])
+                l[1] = self._classes_split(flag_mode, l[1])
+                classes = self._classes_preprocess(af_classes, l[1])
+            # XXX In case we want to support "user dics", we'll have to make
+            #     this a key: value (dict) stuff...
             base_words.append((l[0], classes))
 
-    def gen_words(self, dics=None, minlen=0, maxlen=MAXLEN, unique=False):
+    def gen_words(self, dics=None, minlen=None, maxlen=None, unique=False):
         """
         Yield words, generated from content of self.base_words and
         self.af_classes.
         If dics is not None, it must be an iterable of dic names present in
         self.dics.
-        minlen and maxlen limit minimal/maximal length of generated words.
+        Is not None, minlen and maxlen limit minimal/maximal length of
+        generated words.
         If unique is True, you can be sure it will not yield twice a same word.
         However, this option is heavy on memory (several hundreds of Mo with
         current four dics in dics.zip…)
         """
         words = set()
+        if minlen or maxlen:
+            if not minlen:
+                minlen = 1
+            if not maxlen:
+                maxlen = 32767  # XXX ...
+            bypass_len = False
+        else:
+            bypass_len = True
         if dics == None:
             dics = self.dics
         for k in dics:
             dic = self.dics[k]
             for w, af in dic["base_words"]:
                 # XXX Avoid to yield several times the same word...
-                #     Only per-baseword guard, though.
+                #     Only per-baseword guard, unless unique is set.
                 if not unique or w not in words:
-                    if minlen < len(w) < maxlen:
+                    if bypass_len or minlen < len(w) < maxlen:
                         yield w
                     if unique:
                         words.add(w)
@@ -265,13 +301,19 @@ class Hunspell(object):
                 pfx = []
                 sfx = []
                 comb = []
-                for c1, c2 in af:
-                    for _w in self.apply_class(dic, w, c1, c2):
-                        if minlen < len(_w) < maxlen and _w not in words:
+                for clss in af:
+                    for _w in self.apply_class(dic, w, *clss):
+                        if (bypass_len or minlen < len(_w) < maxlen) and \
+                           _w not in words:
                             yield _w
                             words.add(_w)
 
     def apply_class(self, dic, word, c, *clss):
+        """
+        Applies class c to word, and then recursively calls itself
+        with remaining clss classes, and/or c's own recursive classes.
+        Yeah, recursive nightmare!
+        """
         if clss:
             for _w in self.apply_class(dic, word, *clss):
                 yield _w
@@ -281,21 +323,29 @@ class Hunspell(object):
         for r in c["rules"]:
             if r["if"] and not r["if"].match(word):
                 continue
-            add = r["add"][0]
+            add, rc_clss = r["add"]
             if c["sfx"]:
                 if r["strip"]:
                     _w = word[:-len(r["strip"])] + add
                 else:
                     _w = word + add
-                yield _w
-            else:
+            else:  # Prefix...
                 if r["strip"]:
                     _w = add + word[len(r["strip"]):]
                 else:
                     _w = add + word
-                yield _w
+            yield _w
             # Recursive process of other classes.
             if clss:
-                for _w in self.apply_class(dic, _w, *clss):
-                    yield _w
+                for _r_w in self.apply_class(dic, _w, *clss):
+                    yield _r_w
+            # If this rule recursively uses other rules...
+            # XXX Arg...
+            if rc_clss:
+                for _rc_cls in rc_clss:
+                    for _rc_w in self.apply_class(dic, _w, *_rc_cls):
+                        yield _rc_w
+                        if clss:
+                            for _r_w in self.apply_class(dic, _rc_w, *clss):
+                                yield _r_w
 
